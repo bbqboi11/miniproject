8c05eeb7cad5302fb0744074b6bfd49d6d701e72
bbqboi11 <60369527+bbqboi11@users.noreply.github.com>
2020-02-27 22:53:34 -0800

    updated

--- infection.js
+++ infection.js
@@ -1,35 +1,57 @@
 var speedx
 var speedy
-var ballpositions = [30, 30, ];
-var ballspeed = [2, 2, ];
+var ballpositions = [50, 50, ];
+var ballspeed = [-5, 2, ];
 
 function setup() {
   createCanvas(400, 400);
   colorMode(HSB);
 }
 
-// function preload() {
-//   soundFormats("mp3");
-//   mySound = loadSound("");
-// }
+function preload() {
+  soundFormats("mp3");
+  mySound = loadSound("soundtrack.mp4");
+}
 
 function draw() {
   background(220);
-  noStroke();
+  noStroke()
   fill(132, 52, random(20, 80));
   circle(ballpositions[0], ballpositions[1], 20);
 
   ballpositions[0] = ballpositions[0] + ballspeed[0]
   ballpositions[1] = ballpositions[1] + ballspeed[1]
 
+  //Kill
+
+
+  //Top edg barrier
   if (ballpositions[0] == 10) {
-    ballspeed[0] = 3;
-    ballspeed[1] = 3;
+
+    ballspeed[0] = ballspeed[0] * -1;
   }
 
+  //Bottom edge barrier
+  if (ballpositions[0] == height - 10) {
+    ballspeed[0] = ballspeed[0] * -1;
+  }
 
+  //Right edge barrier
 
+  if (ballpositions[1] == width - 10) {
+    ballspeed[1] = ballspeed[1] * -1;
+  }
 
+  //Left edge barrier
+  if (ballpositions[1] == 10) {
+    ballspeed[1] = ballspeed[1] * -1;
+  }
 
+}
 
+function mousePressed() {
+  if (mouseX - ballpositions[0] < 10 && mouseY - ballpositions[1] < 10){
+ballpositions.splice(0,2)
+  }
+    
 }
